4d6b944285df61b93517fbafa138b49585214974
Carl Törnqvist <carl@tornqv.ist>
2019-01-29 12:20:29 +0100

    Only load prismic toolbar in preview

--- components/prismic-toolbar/index.js
+++ components/prismic-toolbar/index.js
@@ -1,14 +1,26 @@
 var html = require('choo/html')
 
 module.exports = class PrismicToolbar {
+  constructor (id, state, emit) {
+    this.emit = emit
+    this.loaded = false
+    this.enabled = state.preview
+  }
+
+  load () {
+    if (this.loaded) return
+    this.loaded = true
+    window.prismic = window.prismic || {}
+    window.prismic.endpoint = 'https://unga-klara.prismic.io/api/v2'
+    window.document.head.appendChild(html`
+      <script src="//static.cdn.prismic.io/prismic.min.js" async onload=${() => this.emit('render')}></script>
+    `)
+  }
+
   render () {
-    if (typeof window === 'undefined') return null
+    if (!this.enabled || typeof window === 'undefined') return null
+    if (!this.loaded) window.requestAnimationFrame(this.load.bind(this))
     var el = document.getElementById('io-prismic-toolbar')
-    if (!el) return null
-    var proxy = html`<iframe id="io-prismic-toolbar"></iframe>`
-    proxy.isSameNode = function (node) {
-      return node.id === 'io-prismic-toolbar'
-    }
-    return proxy
+    return el || html`<iframe></iframe>`
   }
 }
---
+++ server.js
@@ -0,0 +1,28 @@
+var url = require('url')
+var jalla = require('jalla')
+var { get } = require('koa-route')
+var Prismic = require('prismic-javascript')
+var { resolve } = require('./components/base')
+
+var REPOSITORY = 'https://unga-klara.cdn.prismic.io/api/v2'
+
+var app = jalla('index.js', { sw: 'sw.js' })
+
+app.use(get('/api/prismic-preview', async function (ctx) {
+  var host = process.env.NOW_URL && url.parse(process.env.NOW_URL).host
+  if (host && ctx.host !== host) {
+    return ctx.redirect(url.resolve(process.env.NOW_URL, ctx.url))
+  }
+  var token = ctx.query.token
+  var api = await Prismic.api(REPOSITORY, { req: ctx.req })
+  var href = await api.previewSession(token, resolve, '/')
+  ctx.set('Cache-Control', 'no-cache, private, max-age=0')
+  ctx.redirect(href)
+}))
+
+app.use(function (ctx, next) {
+  ctx.state.preview = ctx.cookies.get(Prismic.previewCookie)
+  return next()
+})
+
+app.listen(8080)
