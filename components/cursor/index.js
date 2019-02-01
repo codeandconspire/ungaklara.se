fdf231c388a3eb1a1a66141b1fdc992bca9f6147
Anton Trollbäck <anton@trollback.se>
2019-02-01 15:34:52 +0100

    Disable custom cursor

--- components/cursor/index.js
+++ components/cursor/index.js
@@ -25,8 +25,8 @@ class Cursor extends Component {
     doc.addEventListener('mousemove', move)
     doc.addEventListener('mousedown', down)
     doc.addEventListener('mouseup', up)
-    doc.addEventListener('mouseleave', leave)
     doc.addEventListener('mouseenter', enter)
+    doc.addEventListener('mouseleave', leave)
 
     // this.unload = doc.removeEventListener('mousemove', move)
   }
@@ -42,7 +42,6 @@ class Cursor extends Component {
           <svg class="Cursor-default" width="17" height="31" viewBox="0 0 17 31">
             <path fill="#fff" fill-rule="evenodd" stroke="#000" stroke-width="2" d="M17.3529412,25.918552 L17.3529412,36 C17.3529412,36.5522847 16.9052259,37 16.3529412,37 L13.6470588,37 C13.0947741,37 12.6470588,36.5522847 12.6470588,36 L12.6470588,25.918552 L9.00624426,27.2208434 C8.48622513,27.4068502 7.91387799,27.1360798 7.72787115,26.6160606 C7.64926245,26.3962944 7.65000276,26.155954 7.72996381,25.9366762 L14.0605147,8.57635768 C14.2497216,8.0574943 14.8237259,7.79025487 15.3425893,7.97946171 C15.6198964,8.08058353 15.8383634,8.29905054 15.9394853,8.57635768 L22.2700362,25.9366762 C22.459243,26.4555396 22.1920036,27.0295439 21.6731402,27.2187507 C21.4538624,27.2987118 21.213522,27.2994521 20.9937557,27.2208434 L17.3529412,25.918552 Z" transform="rotate(-20 -8.85 41.644)"/>
           </svg>
-
         </div>
       </div>
     `
