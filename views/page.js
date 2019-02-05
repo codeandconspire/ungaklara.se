5075a5855348dc773ede19993ee1fec2247b459e
Carl Törnqvist <carl@tornqv.ist>
2019-02-05 11:54:03 +0100

    Add custom blurbs

--- views/page.js
+++ views/page.js
@@ -13,152 +13,199 @@ var Blockquote = require('../components/text/blockquote')
 module.exports = view(page, meta)
 
 function page (state, emit) {
+  // stash of blurbs to aggregate while mapping slices
+  var blurbs = []
+
   return html`
     <main class="View-main">
       <div class="u-container">
         ${state.prismic.getByUID('page', state.params.slug, (err, doc) => {
           if (err) throw err
           if (!doc) return intro.loading()
+
+          var body = []
+          for (let i = 0, len = doc.data.body.length; i < len; i++) {
+            let el = asSlice(doc.data.body[i], i)
+            if (el !== blurbs || i === doc.data.body.length - 1) {
+              if (blurbs.length) {
+                // render aggregated blurbs as grid
+                let opts = { size: { md: '1of2' } }
+                if (blurbs.length === 3 || blurbs.length > 4) {
+                  opts.size.lg = '1of3'
+                }
+                body.push(html`
+                  <div class="u-spaceV8">
+                    ${grid(opts, blurbs)}
+                  </div>
+                `)
+              }
+              if (el !== blurbs) body.push(el)
+              blurbs = []
+            }
+          }
+
           return html`
             <div>
               <div class="u-spaceB8">
                 ${intro({ title: asText(doc.data.title), text: asElement(doc.data.description) })}
               </div>
-              ${doc.data.body.map(function slices (slice, index) {
-                switch (slice.slice_type) {
-                  case 'text': {
-                    if (!slice.primary.text.length) return null
-                    return html`
-                      <div class="Text Text--large u-spaceV6">
-                        ${asElement(slice.primary.text, resolve, serialize)}
-                      </div>
-                    `
-                  }
-                  case 'quote': {
-                    let blockquote = state.cache(Blockquote, `${doc.id}-${index}`)
-                    return html`
-                      <div class="u-spaceV3">
-                        ${blockquote.render({
-                          large: true,
-                          content: asElement(slice.primary.text, resolve, serialize),
-                          caption: asElement(slice.primary.cite, resolve, serialize)
-                        })}
-                      </div>
-                    `
-                  }
-                  case 'image': {
-                    let attrs = {
-                      alt: slice.primary.image.alt || '',
-                      src: slice.primary.image.url,
-                      width: slice.primary.image.dimensions.width,
-                      height: slice.primary.image.dimensions.height
-                    }
-                    return html`
-                      <figure class="Text Text--large u-sizeFull u-spaceV6">
-                        <img ${attrs}>
-                        ${slice.primary.image.copyright ? html`
-                          <figcaption>
-                            <small class="Text-muted">${slice.primary.image.copyright}</small>
-                          </figcaption>
-                        ` : null}
-                      </figure>
-                    `
-                  }
-                  case 'author': {
-                    return html`
-                      <div class="u-spaceV8">
-                        <div class="Text"><hr class="u-spaceB1"></div>
-                        ${byline({
-                          heading: asText(slice.primary.heading),
-                          body: asElement(slice.primary.text, resolve, reset),
-                          image: slice.primary.image.url ? Object.assign({
-                            src: slice.primary.image.url,
-                            alt: slice.primary.image.alt
-                          }, slice.primary.image.dimensions) : null
-                        })}
-                      </div>
-                    `
-                  }
-                  case 'accordion': {
-                    let hasHeading = Boolean(slice.primary.heading.length)
-                    return html`
-                      <section class="u-spaceV8">
-                        ${hasHeading || slice.primary.introduction.length ? html`
-                          <div class="Text Text--large u-spaceB6">
-                            ${hasHeading ? html`
-                              <h2>${asText(slice.primary.heading)}</h2>
-                            ` : null}
-                            ${slice.primary.introduction.length ? asElement(slice.primary.introduction, resolve, serialize) : null}
-                          </div>
-                        ` : null}
-                        <div class="Text u-sizeFull">
-                          ${slice.items.map(function (item) {
-                            if (!item.heading.length) return null
-                            var summary = asText(item.heading)
-                            return html`
-                              <details>
-                                <summary>${hasHeading ? html`<h3>${summary}</h3>` : html`<h2>${summary}</h2>`}</summary>
-                                <div class="Text Text--large">
-                                  ${asElement(item.text)}
-                                </div>
-                              </details>
-                            `
-                          }).filter(Boolean)}
-                        </div>
-                      </section>
-                    `
-                  }
-                  case 'link_list': {
-                    let items = slice.items.filter((item) => item.page.id && !item.page.isBroken)
-                    if (!items.length) return null
-                    let opts = { size: { md: '1of2' } }
-                    if (items.length === 3 || items.length > 4) {
-                      opts.size.lg = '1of3'
-                    }
-                    let heading = asText(slice.primary.heading)
-                    return html`
-                      <div class="u-spaceV8">
-                        ${heading || slice.primary.introduction.length ? html`
-                          <div class="Text Text--large u-spaceB6">
-                            ${heading ? html`<h2>${heading}</h2>` : null}
-                            ${slice.primary.introduction.length ? asElement(slice.primary.introduction, resolve, serialize) : null}
-                          </div>
-                        ` : null}
-                        ${grid(opts, items.map((item) => asCard(item.page, item.color)))}
-                      </div>
-                    `
-                  }
-                  default: return null
-                }
-              })}
+              ${body}
             </div>
           `
         })}
       </div>
     </main>
   `
+
+  // render slice as element
+  // (obj, num) -> Element
+  function asSlice (slice, index) {
+    switch (slice.slice_type) {
+      case 'text': {
+        if (!slice.primary.text.length) return null
+        return html`
+          <div class="Text Text--large u-spaceV6">
+            ${asElement(slice.primary.text, resolve, serialize)}
+          </div>
+        `
+      }
+      case 'heading': {
+        if (!slice.primary.heading.length) return null
+        return html`
+          <div class="Text Text--large u-spaceT8 u-spaceB6">
+            <h2>${asText(slice.primary.heading)}</h2>
+            ${slice.primary.text.length ? asElement(slice.primary.text, resolve, serialize) : null}
+          </div>
+        `
+      }
+      case 'quote': {
+        let blockquote = state.cache(Blockquote, `${state.params.slug}-${index}`)
+        return html`
+          <div class="u-spaceV3">
+            ${blockquote.render({
+              large: true,
+              content: asElement(slice.primary.text, resolve, serialize),
+              caption: asElement(slice.primary.cite, resolve, serialize)
+            })}
+          </div>
+        `
+      }
+      case 'image': {
+        let attrs = {
+          alt: slice.primary.image.alt || '',
+          src: slice.primary.image.url,
+          width: slice.primary.image.dimensions.width,
+          height: slice.primary.image.dimensions.height
+        }
+        return html`
+          <figure class="Text Text--large u-sizeFull u-spaceV6">
+            <img ${attrs}>
+            ${slice.primary.image.copyright ? html`
+              <figcaption>
+                <small class="Text-muted">${slice.primary.image.copyright}</small>
+              </figcaption>
+            ` : null}
+          </figure>
+        `
+      }
+      case 'author': {
+        return html`
+          <div class="u-spaceV6">
+            <div class="Text"><hr class="u-spaceB1"></div>
+            ${byline({
+              heading: asText(slice.primary.heading),
+              body: asElement(slice.primary.text, resolve, reset),
+              image: slice.primary.image.url ? Object.assign({
+                src: slice.primary.image.url,
+                alt: slice.primary.image.alt
+              }, slice.primary.image.dimensions) : null
+            })}
+          </div>
+        `
+      }
+      case 'accordion': {
+        return html`
+          <section class="u-spaceV6">
+            <div class="Text u-sizeFull">
+              ${slice.items.map(function (item) {
+                if (!item.heading.length) return null
+                return html`
+                  <details>
+                    <summary><h3>${asText(item.heading)}</h3></summary>
+                    <div class="Text Text--large">
+                      ${asElement(item.text)}
+                    </div>
+                  </details>
+                `
+              }).filter(Boolean)}
+            </div>
+          </section>
+        `
+      }
+      case 'link_blurb': {
+        let { primary } = slice
+        if (!primary.link.id || primary.link.isBroken) return null
+        blurbs.push(asCard({
+          title: primary.link.data.title,
+          body: primary.link.data.description,
+          image: primary.link.data.featured_image,
+          color: primary.color || primary.link.data.theme,
+          link: {
+            href: resolve(primary.link),
+            text: primary.link.data.cta
+          }
+        }))
+        return blurbs
+      }
+      case 'file_blurb': {
+        let { primary } = slice
+        if (!primary.file.url || primary.file.isBroken) return null
+        blurbs.push(asCard({
+          image: primary.image,
+          title: primary.title,
+          body: primary.text,
+          color: primary.color,
+          link: {
+            href: primary.file.url
+          }
+        }))
+        return blurbs
+      }
+      case 'any_blurb': {
+        let { primary } = slice
+        let { link } = primary
+        if ((!link.url && !link.id) || link.isBroken) return null
+        blurbs.push(asCard({
+          image: primary.image,
+          title: primary.title,
+          body: primary.text,
+          color: primary.color,
+          link: {
+            href: resolve(link),
+            external: link.target === '_blank'
+          }
+        }))
+        return blurbs
+      }
+      default: return null
+    }
+  }
 }
 
 // render linked document as card
 // obj -> Element
-function asCard (doc, color) {
-  var props = {
-    title: asText(doc.data.title),
-    body: asText(doc.data.description),
-    color: color || doc.data.theme,
-    link: {
-      href: resolve(doc),
-      text: doc.data.cta
-    }
-  }
+function asCard (props) {
+  props = Object.assign({}, props, {
+    title: asText(props.title),
+    body: asText(props.body)
+  })
 
-  var image = doc.data.featured_image
-  if (image && image.url) {
+  if (props.image && props.image.url) {
     props.image = {
-      alt: image.alt || '',
-      src: image.url,
-      width: image.dimensions.width,
-      height: image.dimensions.height
+      alt: props.image.alt || '',
+      src: props.image.url,
+      width: props.image.dimensions.width,
+      height: props.image.dimensions.height
     }
   }
 
