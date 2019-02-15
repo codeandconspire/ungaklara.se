b81fcfc4da1bf383076a5d28971443a7ffbe5ea0
Anton Trollbäck <anton@trollback.se>
2019-02-15 18:58:20 +0100

    Tweak startpage

--- components/jigsaw/index.js
+++ components/jigsaw/index.js
@@ -2,9 +2,9 @@ var html = require('choo/html')
 
 module.exports = jigsaw
 
-function jigsaw (heading, body, image, gallery) {
+function jigsaw (heading, body, image, gallery, alt) {
   return html`
-    <div class="Jigsaw">
+    <div class="Jigsaw ${alt ? 'Jigsaw--alt' : null}">
       <div class="u-container">
         <div class="Jigsaw-heading">${heading}</div>
         <div class="Jigsaw-body">${body}</div>
