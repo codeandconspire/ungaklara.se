bc41cb60b9449c0c71f07db0ef942ceceb291ad6
Carl Törnqvist <carl@tornqv.ist>
2023-10-19 13:54:42 +0200

    Fix duplicate keys

--- src/lib/Calendar.svelte
+++ src/lib/Calendar.svelte
@@ -84,7 +84,7 @@
           month: 'long'
         })}
       </time>
-      {#each items as item (item.id)}
+      {#each items as item}
         {@const { event, show } = item}
         {@const start = new Date(show.start)}
         {@const isSoldOut = show.stockStatus === 'SoldOut'}
