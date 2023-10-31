ab4a8130b48dc5c40d35d5d0ef6e2345eeadb0dd
Anton Trollbäck <anton@trollback.se>
2023-10-31 15:20:24 +0100

    Add logo to page footer

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -173,6 +173,7 @@
     {/each}
   </nav>
   <aside>
+    <a class="logo" href="/">Unga Klara</a>
     <ul class="social">
       {#each settings.platforms as item}
         <li class="item">
@@ -183,32 +184,23 @@
             href={resolve(item.link)}>
             <span class="u-hiddenVisually">{item.platform}</span>
             {#if item.platform.toLowerCase().includes('youtube')}
-              <svg x="0" y="0" viewBox="0 0 50 50" fill="currentColor">
+              <svg x="0" y="0" viewBox="0 0 50 50" fill="currentcolor">
                 <path
                   d="M 44.898438 14.5 C 44.5 12.300781 42.601563 10.699219 40.398438 10.199219 C 37.101563 9.5 31 9 24.398438 9 C 17.800781 9 11.601563 9.5 8.300781 10.199219 C 6.101563 10.699219 4.199219 12.199219 3.800781 14.5 C 3.398438 17 3 20.5 3 25 C 3 29.5 3.398438 33 3.898438 35.5 C 4.300781 37.699219 6.199219 39.300781 8.398438 39.800781 C 11.898438 40.5 17.898438 41 24.5 41 C 31.101563 41 37.101563 40.5 40.601563 39.800781 C 42.800781 39.300781 44.699219 37.800781 45.101563 35.5 C 45.5 33 46 29.398438 46.101563 25 C 45.898438 20.5 45.398438 17 44.898438 14.5 Z M 19 32 L 19 18 L 31.199219 25 Z" />
               </svg>
             {/if}
             {#if item.platform.toLowerCase().includes('instagram')}
-              <svg x="0" y="0" viewBox="0 0 30 30" fill="currentColor">
+              <svg x="0" y="0" viewBox="0 0 30 30" fill="currentcolor">
                 <path
                   d="M 9.9980469 3 C 6.1390469 3 3 6.1419531 3 10.001953 L 3 20.001953 C 3 23.860953 6.1419531 27 10.001953 27 L 20.001953 27 C 23.860953 27 27 23.858047 27 19.998047 L 27 9.9980469 C 27 6.1390469 23.858047 3 19.998047 3 L 9.9980469 3 z M 22 7 C 22.552 7 23 7.448 23 8 C 23 8.552 22.552 9 22 9 C 21.448 9 21 8.552 21 8 C 21 7.448 21.448 7 22 7 z M 15 9 C 18.309 9 21 11.691 21 15 C 21 18.309 18.309 21 15 21 C 11.691 21 9 18.309 9 15 C 9 11.691 11.691 9 15 9 z M 15 11 A 4 4 0 0 0 11 15 A 4 4 0 0 0 15 19 A 4 4 0 0 0 19 15 A 4 4 0 0 0 15 11 z" />
               </svg>
             {/if}
             {#if item.platform.toLowerCase().includes('facebook')}
-              <svg x="0" y="0" viewBox="0 0 50 50" fill="currentColor">
+              <svg x="0" y="0" viewBox="0 0 50 50" fill="currentcolor">
                 <path
                   d="M25,3C12.85,3,3,12.85,3,25c0,11.03,8.125,20.137,18.712,21.728V30.831h-5.443v-5.783h5.443v-3.848 c0-6.371,3.104-9.168,8.399-9.168c2.536,0,3.877,0.188,4.512,0.274v5.048h-3.612c-2.248,0-3.033,2.131-3.033,4.533v3.161h6.588 l-0.894,5.783h-5.694v15.944C38.716,45.318,47,36.137,47,25C47,12.85,37.15,3,25,3z" />
               </svg>
             {/if}
-          </a>
-        </li>
-      {/each}
-    </ul>
-    <ul class="terms">
-      {#each settings.terms as item}
-        <li class="item">
-          <a class="link" href={resolve(item.link)}>
-            {item.link_text}
           </a>
         </li>
       {/each}
@@ -321,7 +313,7 @@
 
   .field {
     width: 100%;
-    border: solid currentColor;
+    border: solid currentcolor;
     border-width: var(--border-width) 0 var(--border-width) var(--border-width);
     padding: 0.76rem;
     padding: 0.7rem 1rem;
@@ -406,5 +398,27 @@
   .underline {
     text-decoration: underline;
     text-underline-offset: 0.25em;
+  }
+
+  .logo {
+    font-family: var(--heading-font-family);
+    font-size: 1.75rem;
+    letter-spacing: -0.01em;
+    line-height: 1;
+    text-transform: uppercase;
+    white-space: nowrap;
+    position: relative;
+    top: 0.05em;
+  }
+
+  @media (width > 500px) {
+    .logo {
+      font-size: 2rem;
+      top: 0;
+    }
+  }
+
+  :global(#prismic-toolbar-v2) {
+    display: none !important;
   }
 </style>
