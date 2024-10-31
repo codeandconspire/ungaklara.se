02e52199409fb277022f52475098b062b53242a1
Anton Trollbäck <anton@trollback.se>
2024-10-31 22:11:16 +0100

    Add facebook pixel

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -104,9 +104,46 @@
       dataLayer.push(arguments)
     }
     gtag('js', new Date())
-    gtag('config', 'G-9YLV4PRT5R'${dev ? ', {debug_mode: true}' : ''})`}
+    gtag('config', 'G-9YLV4PRT5R'${dev ? ', {debug_mode: true}' : ''})
+
+    //  Meta Pixel Code
+    !(function (f, b, e, v, n, t, s) {
+      if (f.fbq) return
+      n = f.fbq = function () {
+        n.callMethod
+          ? n.callMethod.apply(n, arguments)
+          : n.queue.push(arguments)
+      }
+      if (!f._fbq) f._fbq = n
+      n.push = n
+      n.loaded = !0
+      n.version = '2.0'
+      n.queue = []
+      t = b.createElement(e)
+      t.async = !0
+      t.src = v
+      s = b.getElementsByTagName(e)[0]
+      s.parentNode.insertBefore(t, s)
+    })(
+      window,
+      document,
+      'script',
+      'https://connect.facebook.net/en_US/fbevents.js'
+    )
+    fbq('init', '1125905244708947')
+    fbq('track', 'PageView')
+    `}
   </svelte:element>
 </svelte:head>
+
+<noscript>
+  <img
+    alt=""
+    height="1"
+    width="1"
+    style="display:none"
+    src="https://www.facebook.com/tr?id=1125905244708947&ev=PageView&noscript=1" />
+</noscript>
 
 {#if !consent}
   <GdprBanner {...gdprProps} on:analytics={handleCookies} />
